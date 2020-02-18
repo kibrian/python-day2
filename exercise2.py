462dd233b443eaaff9db81356ecd2a92b0373a5b
kibrian <bkiprotich39@gmail.com>
2020-02-18 20:47:12 +0300

    big day

---
+++ exercise2.py
@@ -0,0 +1,18 @@
+a=10
+b=6
+c=31
+sum=a+b+c
+def myprinter(myvar):
+    print(myvar)
+
+myprinter('brian')
+
+def add(x,y):
+    sum=x+y
+    print(sum)
+
+add(2,4)
+a=10
+b=12
+promt='a+b'
+def add(x,y)
